2d0e7e1a90dfa110719152e89b04b8b0399bc87a
Everton Cavalcante <evertonranielly@gmail.com>
2022-10-21 19:07:15 -0300

    Updating documentation

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Job"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"PrintingQueue"}];updateSearchResults();
